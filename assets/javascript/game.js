b01ea13903b8388f5b46bb5eb51ab0a7cc326129
Travis Pickering <Pickerings@Traviss-MBP.attlocal.net>
2016-11-11 02:35:56 -0600

    non-functioning reset code added, too sleepy to continue...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,38 +15,83 @@ $(document).ready(function() {
 
 	// create rondomNumber
 	var randomNumber = Math.floor(Math.random() * 100);
+	$("#randomNumber").html(randomNumber);
 	var totalScore = 0;
+	var loss = 0;
+	var wins = 0;
 	var crystal1 = Math.floor(Math.random() * 10);
 	var crystal2 = Math.floor(Math.random() * 20);
 	var crystal3 = Math.floor(Math.random() * 50);
 	var crystal4 = Math.floor(Math.random() * 5);
 
+	
+	// function reset() {
+	// 	var randomNumber = Math.floor(Math.random() * 100);
+	// 	$("#randomNumber").html(randomNumber);
+	// 	var totalScore = 0;
+	// 	var crystal1 = Math.floor(Math.random() * 10);
+	// 	var crystal2 = Math.floor(Math.random() * 20);
+	// 	var crystal3 = Math.floor(Math.random() * 50);
+	// 	var crystal4 = Math.floor(Math.random() * 5);
+	// };
+
 	$("#crystal1").on("click", function() {
 		// crystal1 = Math.floor(Math.random() * 10);
 		totalScore = totalScore + crystal1;
 		$("#totalScore").html(totalScore);
-		// if (totalScore  
+		
+		if (totalScore > randomNumber) {
+			loss ++;
+			$("#loss").html("Losses: " + loss);
+			reset();
+		} else if (totalScore == randomNumber) {
+			wins ++;
+			$("#wins").html("Win total: " + wins);
+			reset();
+		};
+
 
 	})
 
 	$("#crystal2").on("click", function() {
 		totalScore = totalScore + crystal2;
 		$("#totalScore").html(totalScore);
-		// if (totalScore  
+		
+		if (totalScore > randomNumber) {
+			loss ++;
+			$("#loss").html("Losses: " + loss);
+		} else if (totalScore == randomNumber) {
+			wins ++;
+			$("#wins").html("Win total: " + wins);
+		}; 
 
 	})
 
 	$("#crystal3").on("click", function() {
 		totalScore = totalScore + crystal3;
 		$("#totalScore").html(totalScore);
-		// if (totalScore  
+		
+		if (totalScore > randomNumber) {
+			loss ++;
+			$("#loss").html("Losses: " + loss);
+		} else if (totalScore == randomNumber) {
+			wins ++;
+			$("#wins").html("Win total: " + wins);
+		}; 
 
 	})
 
 	$("#crystal4").on("click", function() {
 		totalScore = totalScore + crystal4;
 		$("#totalScore").html(totalScore);
-		// if (totalScore  
+		
+		if (totalScore > randomNumber) {
+			loss ++;
+			$("#loss").html("Losses: " + loss);
+		} else if (totalScore == randomNumber) {
+			wins ++;
+			$("#wins").html("Win total: " + wins);
+		}; 
 
 	})
 	console.log(totalScore);
