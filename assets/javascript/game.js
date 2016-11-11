2a72cf5774d77d6fc1d3bffbb812532a08245dec
Travis Pickering <Pickerings@Traviss-MBP.attlocal.net>
2016-11-11 01:19:08 -0600

    add buttons to html, images, and create randomNum variable

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+// Psuedocode
+
+// Create an object with the following:
+// var randomNumber: Generate a random number at start of game 
+// button1
+// button2
+// button3
+// button4
+// Add button click value to total score
+// If total score equals random number, you win. If total score equals beyond random number you lose.
+// reset game either way
+// How to get game to reset? 
+
+$(document).ready(function() {
+
+	// create rondomNumber
+	var randomNumber = Math.floor(Math.random() * 100);
+	console.log(randomNumber);
+
+});
+
