d392b165c7018f8ecde6229c2eeeae3e57e4e0db
Travis Pickering <Pickerings@Traviss-MBP.attlocal.net>
2016-11-11 02:04:25 -0600

    crystals now count and add to totalScore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,7 +15,44 @@ $(document).ready(function() {
 
 	// create rondomNumber
 	var randomNumber = Math.floor(Math.random() * 100);
-	console.log(randomNumber);
+	var totalScore = 0;
+	var crystal1 = Math.floor(Math.random() * 10);
+	var crystal2 = Math.floor(Math.random() * 20);
+	var crystal3 = Math.floor(Math.random() * 50);
+	var crystal4 = Math.floor(Math.random() * 5);
+
+	$("#crystal1").on("click", function() {
+		// crystal1 = Math.floor(Math.random() * 10);
+		totalScore = totalScore + crystal1;
+		$("#totalScore").html(totalScore);
+		// if (totalScore  
+
+	})
+
+	$("#crystal2").on("click", function() {
+		totalScore = totalScore + crystal2;
+		$("#totalScore").html(totalScore);
+		// if (totalScore  
+
+	})
+
+	$("#crystal3").on("click", function() {
+		totalScore = totalScore + crystal3;
+		$("#totalScore").html(totalScore);
+		// if (totalScore  
+
+	})
+
+	$("#crystal4").on("click", function() {
+		totalScore = totalScore + crystal4;
+		$("#totalScore").html(totalScore);
+		// if (totalScore  
+
+	})
+	console.log(totalScore);
+
+
+
 
 });
 
